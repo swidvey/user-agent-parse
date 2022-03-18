758898fdf4e17488de62b2172b881390f774ee59
swidvey <swidvey@yahoo.com>
2022-03-18 15:13:43 -0600

    updated files

---
+++ initiate_app.sh
@@ -0,0 +1,5 @@
+cd ~
+cd .../user_agent_parse/
+python3 -m venv ua_ve
+source ua_ve/bin/activate
+pip install -r requirements.txt
---
+++ user_agent_bash.sh
@@ -0,0 +1,4 @@
+cd ~
+cd .../user_agent_parse
+source ua_ve/bin/activate
+python3 main.py -b Y
