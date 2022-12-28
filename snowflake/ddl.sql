1f76e7a727f02c1072ef22848cc382c5040da2a1
swidvey <swidvey@yahoo.com>
2022-12-28 15:13:35 -0700

    ddl update

--- snowflake/ddl.sql
+++ snowflake/ddl.sql
@@ -1,6 +1,3 @@
-
--- to Do: ADD VARCHAR size. These are max value
-
 ---------------------------------------------------
 ---------------------------------------------------
 -- User Agent Parse Tables
@@ -9,10 +6,14 @@
 --      Step 1) Create Data Base and Schema
 --      Step 2) Create stage table
 --      Step 3) Create main table
+--      Step 4) Create data table
 
 -- note: there is no copy statment. 
 -- The python program writes into snowflake.
--- Main Tables need to be created first 
+-- Main Tables need to be created first.
+
+-- It is assumed you have a snowflake table with UA data
+-- See example on USERSTACK 
 ---------------------------------------------------
 ------------         Step 1            ------------
 ---------------------------------------------------
@@ -25,33 +26,33 @@ CREATE SCHEMA USERSTACK;
 ---------------------------------------------------
 
 CREATE OR REPLACE TABLE USERSTACK.STAGE.USER_AGENT_MAPPER(
-      user_agent                VARCHAR()
-    , ua_type                   VARCHAR()
-    , ua_brand                  VARCHAR()
-    , ua_name                   VARCHAR()
-    , ua_url                    VARCHAR()
-    , os_name                   VARCHAR()  
-    , os_code                   VARCHAR()  
-    , os_url                    VARCHAR()  
-    , os_family                 VARCHAR()  
-    , os_family_code            VARCHAR()
-    , os_family_vendor          VARCHAR()    
-    , os_icon                   VARCHAR()    
-    , os_icon_large             VARCHAR()
-    , is_mobile_device          VARCHAR()
-    , device_type               VARCHAR()
-    , device_brand              VARCHAR()
-    , device_brand_code         VARCHAR()
-    , device_brand_url          VARCHAR()
-    , device_name               VARCHAR()
-    , browser_name              VARCHAR()
-    , browser_version           VARCHAR()
-    , browser_version_major     VARCHAR()
-    , browser_engine            VARCHAR()
-    , is_crawler                VARCHAR()
-    , crawler_category          VARCHAR()
-    , crawler_last_seen         VARCHAR()
-    , LOAD_TIMESTAMP            VARCHAR()
+      user_agent                VARCHAR(5000)
+    , ua_type                   VARCHAR(100)
+    , ua_brand                  VARCHAR(100)
+    , ua_name                   VARCHAR(100)
+    , ua_url                    VARCHAR(5000)
+    , os_name                   VARCHAR(5000)  
+    , os_code                   VARCHAR(5000)  
+    , os_url                    VARCHAR(5000)  
+    , os_family                 VARCHAR(5000)  
+    , os_family_code            VARCHAR(5000)
+    , os_family_vendor          VARCHAR(5000)    
+    , os_icon                   VARCHAR(5000)    
+    , os_icon_large             VARCHAR(5000)
+    , is_mobile_device          VARCHAR(10)
+    , device_type               VARCHAR(50)
+    , device_brand              VARCHAR(100)
+    , device_brand_code         VARCHAR(50)
+    , device_brand_url          VARCHAR(5000)
+    , device_name               VARCHAR(50)
+    , browser_name              VARCHAR(50)
+    , browser_version           VARCHAR(50)
+    , browser_version_major     VARCHAR(500)
+    , browser_engine            VARCHAR(500)
+    , is_crawler                VARCHAR(10)
+    , crawler_category          VARCHAR(50)
+    , crawler_last_seen         VARCHAR(50)
+    , LOAD_TIMESTAMP            DATETIME
 );
 
 
@@ -59,32 +60,31 @@ CREATE OR REPLACE TABLE USERSTACK.STAGE.USER_AGENT_MAPPER(
 ------------         Step 3            ------------
 ---------------------------------------------------
 CREATE OR REPLACE TABLE USER_DATA.USERSTACK.USER_AGENT_MAPPER(
-      user_agent                VARCHAR()
-    , ua_type                   VARCHAR()
-    , ua_brand                  VARCHAR()
-    , ua_name                   VARCHAR()
-    , ua_url                    VARCHAR()
-    , os_name                   VARCHAR()  
-    , os_code                   VARCHAR()  
-    , os_url                    VARCHAR()  
-    , os_family                 VARCHAR()  
-    , os_family_code            VARCHAR()
-    , os_family_vendor          VARCHAR()    
-    , os_icon                   VARCHAR()    
-    , os_icon_large             VARCHAR()
-    , is_mobile_device          VARCHAR()
-    , device_type               VARCHAR()
-    , device_brand              VARCHAR()
-    , device_brand_code         VARCHAR()
-    , device_brand_url          VARCHAR()
-    , device_name               VARCHAR()
-    , browser_name              VARCHAR()
-    , browser_version           VARCHAR()
-    , browser_version_major     VARCHAR()
-    , browser_engine            VARCHAR()
-    , is_crawler                VARCHAR()
-    , crawler_category          VARCHAR()
-    , crawler_last_seen         VARCHAR()
-    , LOAD_TIMESTAMP            VARCHAR() 
+      user_agent                VARCHAR(5000)
+    , ua_type                   VARCHAR(100)
+    , ua_brand                  VARCHAR(100)
+    , ua_name                   VARCHAR(100)
+    , ua_url                    VARCHAR(5000)
+    , os_name                   VARCHAR(5000)  
+    , os_code                   VARCHAR(5000)  
+    , os_url                    VARCHAR(5000)  
+    , os_family                 VARCHAR(5000)  
+    , os_family_code            VARCHAR(5000)
+    , os_family_vendor          VARCHAR(5000)    
+    , os_icon                   VARCHAR(5000)    
+    , os_icon_large             VARCHAR(5000)
+    , is_mobile_device          VARCHAR(10)
+    , device_type               VARCHAR(50)
+    , device_brand              VARCHAR(100)
+    , device_brand_code         VARCHAR(50)
+    , device_brand_url          VARCHAR(5000)
+    , device_name               VARCHAR(50)
+    , browser_name              VARCHAR(50)
+    , browser_version           VARCHAR(50)
+    , browser_version_major     VARCHAR(500)
+    , browser_engine            VARCHAR(500)
+    , is_crawler                VARCHAR(10)
+    , crawler_category          VARCHAR(50)
+    , crawler_last_seen         VARCHAR(50)
+    , LOAD_TIMESTAMP            DATETIME
 );
-	
